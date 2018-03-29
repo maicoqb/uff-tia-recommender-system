3f70ce603a98d432427fd5f6939db10c3506a3bb
Maico Borges <maicoqb@gmail.com>
2018-03-28 22:16:22 -0300

    Criando main e adicionando argumentos para a execução do script

---
+++ main.py
@@ -0,0 +1,21 @@
+import argparse
+
+APPLICATION_NAME="recommender"
+APPLICATION_DESCRIPTION="""
+Sistema de Recomendação
+"""
+
+def main(args):
+    pass
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog=APPLICATION_NAME, description=APPLICATION_DESCRIPTION)
+    parser.add_argument('arquivo', metavar="ARQUIVO", type=str, 
+                        help="Arquivo csv processado para gerar as recomendações")
+    parser.add_argument('usuario', metavar="USUARIO", type=int, 
+                        help="Indice do usuário que se deseja a avaliação do ITEM")
+    parser.add_argument('item',    metavar="ITEM",    type=int, 
+                        help="Indice do item que se deseja a avaliação do USUARIO")
+    args = parser.parse_args()
+    
+    main(args)
